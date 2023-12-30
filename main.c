c818fa4c3e522c091d03e22b4d1f2acf79ef25c2
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2023-12-30 14:40:38 +0100

    Added the creation and the search function
    Added some minor functions including creation and search functions, moved the structs and global variables to another files to improve the organization of the code

--- main.c
+++ main.c
@@ -1,42 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include "structs.h"
 
-// The declaration of the header of a file
-typedef struct fileHeader{
-    unsigned short NbStructs;
-    char name[50];
-}fileHeader;
+block* allocBlock(){
+    for(int i = 0; i < 16; i++)
+        if(MS[i].isUsed == 0){
+            MS[i].isUsed = 1;
+            return &MS[i];
+        }
+    
+    return NULL; 
+}
 
-// The declaration of the header of a block
-typedef struct blockHeader{
-    unsigned short NbStructs;  
-    unsigned short remainingSpace;
-}blockHeader;
+bool __recuSearch(unsigned short startIndex, unsigned short endIndex, char* key){
+    if(startIndex == endIndex)
+        if(Index[startIndex].isDeletedLogically)
+            return false;
+    
+    unsigned short median = (startIndex + endIndex) / 2;
+    int strcmpResult = strncmp(key, Index[median].key, KEY_MAX_SIZE);
 
-// The declaration of a single block, which contains multiple structs
-typedef struct block{
-    blockHeader header;
-    char tab[100];
-    block *next;
-}block;
+    if(startIndex == endIndex && strcmpResult)
+        return false;
 
-// The declaration of the file, which will be a sorted linked list, with variable size and no overlap (chevauchement), "head" 
-// will be the start of the linked list.
-typedef struct fichier{
-    fileHeader header;
-    block* head;
-}fichier;
+    if(!strcmpResult)       return true;
+    if(strcmpResult < 1)
+        __recuSearch(startIndex, median, key);
+    else
+        __recuSearch(median + 1, endIndex, key);
+}
 
-// The declaration of an element of the "index" array, a pointer to the key. and 2 pointers to the block and the end of the 
-// struct of the key
-typedef struct IndexElement{        
-    char *key;
-    block *blockAddress;
-    char *endAddress;
-    bool isDeletedLogically;
-}IndexElement;                  
+bool searchElement(){
+    // char *key = (char*)malloc(sizeof(KEY_MAX_SIZE + 1));
+    // if(key == NULL){
+    //     fprintf(stderr, "ERROR! [malloc in searchElement]: Couldn't allocate memory")
+    //      exit(EXIT_FAILURE);
+    // }
+    // key = strncpy(key, buffer.tab, KEY_MAX_SIZE);
+    if(indexSize == 0)                  return false;
+    return __recuSearch(0, indexSize - 1, buffer.tab);
+}
 
-// Global variables
-block *buffer;                      // A buffer to transfer data between RAM and Memory (used for file manipulation operations)
-IndexElement index[50];             // An index associated to the file containing useful information for various operations
+void createFile(file* file){
+    file->head = (fBlock*)malloc(sizeof(fBlock));
+    file->head->data = allocBlock();
+    if(file->head->data == NULL){
+        fprintf(stderr, "\nERROR! [allocBlock in createFile]: No space to allocate block.\nExiting...\n");
+        exit(EXIT_FAILURE);
+    }
+    file->head->next = NULL;
+    file->header.NbStructs = 0;
+    printf("Enter the name of the file (max size is 35 characters, spaces NOT allowed): ");
+    scanf("%36s", file->header.name);
+}
+
+int main(int argc, char const *argv[]){
+    unsigned short answer;                                  // Used to get user's answers
+    file file;
+    memset(&file, 0, sizeof(file));
+    
+    printf("Do you want to create a new file?\n");
+    printf("1- yes\n");
+    printf("2- no\n");
+    scanf("%hu", &answer);                                  // "%hu" format specifier for unsigned short
+    if(answer == 1)
+        createFile(&file);
+    else
+        goto end;
+
+    do{
+        memset(&buffer, sizeof(buffer), 0);                 // Clear the buffer for future uses.
+
+        printf("Enter what you want to do in numbers\n");
+        printf("1- Insert an element\n");
+        printf("2- Delete an element\n");
+        printf("3- Search for an element\n");
+        printf("4- Display contents of file\n");
+
+        scanf("%hu", &answer);
+        switch (answer)
+        {
+        case 1:
+            // Insert function
+            break;
+        
+        case 2:
+            // Delete function
+            break;
+
+        case 3:
+            printf("Enter the key to search (Keys does NOT contain spaces) and a maximum size of %d: ", KEY_MAX_SIZE - 1);
+            scanf("%16s", buffer.tab);
+            if( searchElement())
+                printf("Element exists.\n");
+            else
+                printf("Element doesn't exist.\n");
+            
+            break;
+
+        case 4:
+            // Print function
+            break;
+
+        default:
+            fprintf(stderr, "Invalid choice, exiting...");
+            exit(EXIT_FAILURE);
+            break;
+        }
+
+        printf("Want to leave or do another thing?\n");
+        printf("1- yes\n");
+        printf("2- no\n");
+        scanf("%hu", &answer);
+    }while(answer == 1);
+
+    end:
+        printf("\nExiting...\n");
+        return 0;
+}
---
+++ structs.h
@@ -0,0 +1,46 @@
+#define KEY_MAX_SIZE 16
+
+// The declaration of the header of a file.
+typedef struct fileHeader{
+    unsigned int NbStructs;
+    char name[36];
+}fileHeader;
+
+// The declaration of the header of a block.
+typedef struct blockHeader{
+    unsigned short NbStructs;  
+    unsigned short usedSpace;
+}blockHeader;
+
+// The declaration of a single block in memory, which is capable of containing multiple structs.
+typedef struct block{
+    blockHeader header;                     // The header of the block, containing useful/essential information.
+    char tab[50];                           // The actual memory of the block.
+    bool isUsed;                            // A boolean that indicates the availability of the block, needed for insertion into a new block.
+}block;
+
+// The declaration of a single block in memory when used by a the file declared below.
+typedef struct fBlock{
+    block* data;                             // The actual block struct in memory.
+    block* next;                            // A pointer to the next element (block) in the linked list.
+}fBlock;
+
+// The declaration of the file, which will be a sorted linked list, with variable size and no overlap (chevauchement).
+typedef struct file{
+    fileHeader header;                      // The header of the file, containing useful/essential information.
+    fBlock *head;                           // The head of the linked list containing all the data of the file.
+}file;
+
+// The declaration of an element of the "index" array, which contains information needed for search, insertion, ...etc.
+typedef struct indexElement{        
+    char *key;
+    block *blockAddress;                    // Pointer to the block of the element.
+    char *endAddress;                       // Pointer to the last byte of the element.
+    bool isDeletedLogically;                // Whether the element is deleted logically or not. An element which is deleted logically will be ignored.
+}indexElement;                  
+
+// Global variables
+block buffer;                               // A buffer to transfer data between RAM and Memory (used for file manipulation operations).
+indexElement Index[36];                     // An index associated to the file containing useful information for various operations.
+unsigned short indexSize = 0;               // The index of the first free element in the index.
+block MS[16];                               // The Memory which will contain all the blocks of the linked list and other blocks used by default.
