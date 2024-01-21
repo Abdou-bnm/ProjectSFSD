acac10a898c93da0f85ce7dd16f21c745b39a435
chahinez-hdd <chahinez.haddad2005@gmail.com>
2024-01-22 00:27:01 +0100

    rectification struct.h

--- structs.h
+++ structs.h
@@ -1,3 +1,6 @@
+#ifndef PRIMARY_HEADER_H
+#define PRIMARY_HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -6,11 +9,15 @@
 #define KEY_MAX_SIZE 16
 #define BUFFER_MAX_SIZE 50
 #define INDEX_ELEMENT_MAX 36
-#include <stdbool.h>
+#define Memory_Bloc_Max 16
+
+/// Part of Struct Declaration -----------------------------------------------------------------------------------
+
 // The declaration of the header of a file.
 typedef struct fileHeader{
-    unsigned int NbStructs;
-    char name[36];
+    unsigned int nbBlocks;                  // Number of blocks used by file
+    unsigned int NbStructs;                 // Number of structs in file
+    char name[36];                          // Name of file, no path included
 }fileHeader;
 
 // The declaration of the header of a block.
@@ -25,20 +32,22 @@ typedef struct blockHeader{
 // The declaration of a single block in memory, which is capable of containing multiple structs.
 typedef struct block{
     blockHeader header;                     // The header of the block, containing useful/essential information.
-    char tab[BUFFER_MAX_SIZE];                           // The actual memory of the block.
+    char tab[BUFFER_MAX_SIZE];              // The actual memory of the block.
     bool isUsed;                            // A boolean that indicates the availability of the block, needed for insertion into a new block.
 }block;
 
 // The declaration of a single block in memory when used by a the file declared below.
 typedef struct fBlock{
-    block *data;                             // The actual block struct in memory.
-   struct fBlock *next;                            // A pointer to the next element (block) in the linked list.
+    block* data;                            // The actual block struct in memory.
+    struct fBlock* next;                    // A pointer to the next element (block) in the linked list.
 }fBlock;
 
 // The declaration of the file, which will be a sorted linked list, with variable size and no overlap (chevauchement).
 typedef struct file{
     fileHeader header;                      // The header of the file, containing useful/essential information.
     fBlock *head;                           // The head of the linked list containing all the data of the file.
+    FILE* RFile;                            // The permanent .txt file that the data will be written to
+    FILE* HFile;                            // File where we will store file and blocks headers, raw binary data
 }file;
 
 // The declaration of an element of the "index" array, which contains information needed for search, insertion, ...etc.
@@ -47,6 +56,7 @@ typedef struct indexElement{
     fBlock *blockAddress;                   // Pointer to the block of the element.
     char *endAddress;                       // Pointer to the last byte of the element.
     bool isDeletedLogically;                // Whether the element is deleted logically or not. An element which is deleted logically will be ignored.
+    unsigned long filePos;                  // Position of 1st character of key in RFile, providing that the 1st postion in RFile is 0. 
 }indexElement;                  
 
 typedef struct IndexType{
@@ -54,38 +64,42 @@ typedef struct IndexType{
     unsigned short IndexSize;  
 }IndexType;
 
-char defiler(char file[2*BUFFER_MAX_SIZE],int *Endfile)
-{
-    char car = file[0];
-    for(int i=0;i<99;i++)
-    {
-        *(file+i) = *(file+i+1);
-    }
-    (*Endfile)--;
-    return car;
-}
+///------------------------------------------------------------------------------------------------------------------------
 
-void enfiler(char file[2*BUFFER_MAX_SIZE],char car,int *Endfile)
-{
-        *(file + *Endfile) = car;
-        (*Endfile)++;
-}
-char *EndKey(char *Key){
-char *KeyPrime=Key;
-// printf("\nkey:%p",Key);
+/// Part of function Declaration ------------------------------------------------------------------------------------------
 
-// printf("\nkeyprime:%p\n",KeyPrime);
-while(*KeyPrime!='\0'){
-    KeyPrime++;
-//printf("whilekey");
-}
-return(KeyPrime-1);
-}
+char defiler(char file[2*BUFFER_MAX_SIZE],int *Endfile);
+void enfiler(char file[2*BUFFER_MAX_SIZE],char car,int *Endfile);
+block* allocBlock();
+void createfile(file* file);
+short __recuSearch(unsigned short startIndex, unsigned short endIndex, char* key);
+short searchElement();
+fBlock *insertion(fBlock *head,char TabKey[KEY_MAX_SIZE],int SizeTabKey,int SizeTabRest);
+void ElementShift(char** NewElementPos,char* StartCurElementPos,char* EndCurElementPos);
+int CalculateSpace(char *StartEspaceAddress, char *EndEspaceAddress);
+void UpdateIndexDelete(int IndexElementDeleted);
+void UpdateFileStruct(file* file);
+void DeleteElementLogique(FILE* RFile);
+int DeleteElementPhysique(file *file,FILE *Rfile);
+void decimalPrint(char* src, int size);
+void __printBlock(block* block);
+void printFile(file file);
+void printIndex();
+void RFile_insert(FILE* file, char* element, unsigned long elementSize);
+void __setBlockAtStart(block* blck);
+void fileOpen(file* file);
+void StockHeaderecFile(FILE *Recfile, file *file);
+void loadHeader(file *file);
+void RFile_Delete(FILE *file, short elementIndexPos);
+void updateIndexFPos(FILE* file);
+/// -----------------------------------------------------------------------------------------------------------------------
 
+/// Part of Variable Declaration ------------------------------------------------------------------------------------------
 
 // Global variables
-char buffer[BUFFER_MAX_SIZE];                               // A buffer to transfer data between RAM and Memory (used for file manipulation operations).
-block MS[16];                               // The Memory which will contain all the blocks of the linked list and other blocks used by default.
-IndexType Index;                            // An index associated to the file containing useful information for various operations.
-block MS[16];                               // The Memory which will contain all the blocks of the linked list and other blocks used by default.
-IndexType Index;
+extern char buffer[BUFFER_MAX_SIZE];               // A buffer to transfer data between RAM and Memory (used for file manipulation operations).
+extern block MS[Memory_Bloc_Max];                               // The Memory which will contain all the blocks of the linked list and other blocks used by default.
+extern IndexType Index;                            // An index associated to the file containing useful information for various operations.
+/// -----------------------------------------------------------------------------------------------------------------------
+
+#endif
